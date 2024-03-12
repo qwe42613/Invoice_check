64c731aa119e196227b3c0da8c6d6ba14f7b2a53
Sheng <mac@Big99deMacBook-Pro.local>
2024-03-12 14:32:21 +0800

    crawl test

---
+++ Incoice_check.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://invoice.etax.nat.gov.tw/index.html"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+td=soup.select('.container-fluid')[0].select('.etw-tbiggest')
+special_reward = td[0].getText()
+first_reward = td[1].getText()
+print(first_reward)
